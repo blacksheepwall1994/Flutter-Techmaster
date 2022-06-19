caa4c691234746e8309d2c54573a463c9044eba7
blacksheepwall1994 <blacksheepwall1994@gmail.com>
2022-06-20 00:05:40 +0700

    fix practice1

--- practice1.dart
+++ practice1.dart
@@ -1,9 +1,9 @@
 void main(List<String> args) {
   String str =
       " đây là kết quả của buổi học thứ 2 về dart: dart basics (phần 1)...";
-  String str1 = str.trim().substring(37, 54).toUpperCase();
+  String str1 = str.trim().substring(43, 54).toUpperCase();
   String str2 =
-      "${str.trim()[0].toUpperCase()}${str.trim().substring(1, 37)}${str1.toString()}${str.trim().substring(54, 63)}";
+      "${str.trim()[0].toUpperCase()}${str.trim().substring(1, 37)}${str.trim().substring(37, 38).toUpperCase()}${str.trim().substring(37, 43)}${str1}${str.trim().substring(54, 63)}";
   print(str2);
 
   /// hãy viết code bằng tất cả các cách có thể để in ra:
